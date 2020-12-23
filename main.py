5ccf4d1d0038fa817799a787e739c1dc1c84904e
isaac-18 <isaac.curiel6721@gmail.com>
2020-12-22 23:15:03 -0800

    Functioning command line input

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import os
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-APP_CLIENT_ID = ""
-APP_CLIENT_SECRET = ""
+APP_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+APP_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=APP_CLIENT_ID,
                                                            client_secret=APP_CLIENT_SECRET))
 
-album_url = 'https://open.spotify.com/album/10D2HYrLcMJj2QPJt02Adq?si=Uzu3DbwPSNCvD4am9mBUbw'
-results = sp.album(album_url)
+search_type = int(input("Would like the art from an album (1) or song (2): "))
 
-album_name = results['name'].replace(' ', '_')
-image_url = results['images'][0]['url']
+url = input("Enter the album/song link: ")
+
+if search_type == 1:
+    results = sp.album(url)
+    item_name = results['name'].replace(' ', '_')
+    image_url = results['images'][0]['url']
+elif search_type == 2:
+    results = sp.track(url)
+    item_name = results['name'].replace(' ', '_')
+    image_url = results['album']['images'][0]['url']
+else:
+    print("Error: invalid input")
+
 
 img_data = requests.get(image_url).content
-with open("C:/Users/Isaac/Pictures/album_covers/{}.jpg".format(album_name), 'wb') as handler:
+with open("C:/Users/Isaac/Pictures/album_covers/{}.jpg".format(item_name), 'wb') as handler:
     handler.write(img_data)
